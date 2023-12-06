e2e611e22469b687037003099abe7e39db28c593
root <root@SALIHA-BRIK.localdomain>
2023-12-06 23:42:02 +0100

    update

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+// JavaScript code in script.js
+
 const sisi = document.querySelector(".sisi");
 const navMenu = document.querySelector(".nav-menu");
 
-sisi.addEventListener("click",()=>{
+sisi.addEventListener("click", () => {
     sisi.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-link").forEach(n =>n.addEventListener("click",()=>{
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     sisi.classList.remove("active");
     navMenu.classList.remove("active");
-    
 }));
 
 let searchInput = document.getElementById("searchInput");
 
-
-
-searchInput.placeholder = "Enter medicine name!"
+searchInput.placeholder = "Enter medicine name!";
 searchInput.style.width = "300px";
 searchInput.style.height = "40px";
 
+// Define toggleMenu globally
+window.toggleMenu = function() {
+    sisi.classList.toggle("active");
+    navMenu.classList.toggle("inactive");
+};
 
-
-//let searchButton = document.getElementById("searchButton");
 searchInput.addEventListener("click", () => {
-    alert("done")
-})
+    alert("Sorry there is drug");
+});
